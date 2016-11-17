cd1739eb8c07698bbb54ad3b7fb89ea6e58c6969
buec <buec@zhaw.ch>
2016-11-17 23:10:00 +0100

    prepared docs for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
